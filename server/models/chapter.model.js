d79385e8ea137aaaf0ac06a156908b477df27202
izzahaj <izzah.jalil02@gmail.com>
2023-03-04 17:24:44 +0800

    Add Chapter model

---
+++ server/models/chapter.model.js
@@ -0,0 +1,28 @@
+const Chapter = (sequelize, Sequelize) => {
+  const chapter = sequelize.define("chapter", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    order: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    body: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    }
+  })
+  return chapter;
+}
+
+export default Chapter;
